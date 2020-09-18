8132710b947d4568909a8f9e6986d13da5995466
jjp6315 <jjp6315@psu.edu>
2020-09-18 22:45:41 +0000

    d

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 # Author: Ji Woong Park jjp6315@psu.edu
 # Section: 005R
 
-def sum_n(n):
+def digit_sum(n):
   if n == 0:
     return n
   else: 
     n1 = n % 10
     n2 = n // 10
+    n3 = n // 100
     return n1 + n2
   
 
 def run():
   n = input("Enter an int: ")
   n = int (n)
-  print(f"sum of digits of a number is: {sum_n(n)} ")
+  print(f"sum of digits of a number is: {digit_sum(n)} ")
  
 
 
